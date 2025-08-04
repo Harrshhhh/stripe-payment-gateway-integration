1034045304103a0155ec0e7804c25773250b83ab
Harsh Phartiyal <harshphartiyal20@gmail.com>
2025-08-04 16:21:31 +0530

    Initial Spring Boot project structure

---
+++ paymentIntegrationSystem/src/main/java/com/harsh/paymentIntegrationSystem/PaymentIntegrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.paymentIntegrationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrationSystemApplication.class, args);
+	}
+
+}
---
+++ paymentIntegrationSystem/src/test/java/com/harsh/paymentIntegrationSystem/PaymentIntegrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.paymentIntegrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
